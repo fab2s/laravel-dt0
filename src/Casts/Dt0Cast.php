ccb8d4477d7f0f1287ff7d5793bc93c821f67e8f
fab2s <fabrice.de.stefanis@gmail.com>
2024-04-28 17:57:37 +0200

    Fix pint header

--- src/Casts/Dt0Cast.php
+++ src/Casts/Dt0Cast.php
@@ -2,7 +2,7 @@
 
 /*
  * This file is part of fab2s/laravel-dt0.
- * (c) Fabrice de Stefanis / https://github.com/fab2s/dt0
+ * (c) Fabrice de Stefanis / https://github.com/fab2s/laravel-dt0
  * This source file is licensed under the MIT license which you will
  * find in the LICENSE file or at https://opensource.org/licenses/MIT
  */
